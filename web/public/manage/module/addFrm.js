7a6f50c65902fd0dea36f3b25aa18d24d3bb19f9
3203317 <3203317@qq.com>
2014-05-30 15:10:11 +0800

    haha

--- web/public/manage/module/addFrm.js
+++ web/public/manage/module/addFrm.js
@@ -2,8 +2,8 @@ function valiAddFrm(data){
 	if(!data) return '参数异常';
 
 	if(!data.PId || !/^(0|[a-zA-Z]\w{31})$/.test(data.PId)) return ['不符合格式要求。', 'PId'];
-	if(!data.ModuleName || !/^\d+$/.test(data.ModuleName)) return ['模块名称不符合格式要求。', 'ModuleName'];
-	if(!data.Sort || !/^\d+$/.test(data.Sort)) return ['排序不符合格式要求。', 'Sort'];
+	if(!data.ModuleName || !/^[\u4E00-\u9FA5\w]{1,10}$/.test(data.ModuleName)) return ['仅支持1-10位中文、数字、字母或下划线。', 'ModuleName'];
+	if(!data.Sort || !/^\d+$/.test(data.Sort)) return ['仅支持数字。', 'Sort'];
 }
 
 exports.validate = valiAddFrm;
