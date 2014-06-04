3d0c9ff0870a73ee13d195c6b49fccca18752786
3203317 <3203317@qq.com>
2014-06-04 18:22:30 +0800

    haha

--- android/hyx/src/test/java/com/zhaobe/hyx/UserServiceTest.java
+++ android/hyx/src/test/java/com/zhaobe/hyx/UserServiceTest.java
@@ -2,10 +2,13 @@ package com.zhaobe.hyx;
 
 import org.junit.Test;
 
+import com.zhaobe.hyx.db.DBManager;
+
 public class UserServiceTest {
 
 	@Test
 	public void login() {
 		System.out.println(121);
+		DBManager dbMgr = new DBManager(null);
 	}
 }
