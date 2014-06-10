9140739139f253d5366db5186f2720afd3d0da59
3203317 <3203317@qq.com>
2014-06-10 14:01:13 +0800

    haha

--- android/hyx/src/com/zhaobe/hyx/activity/LoginActivity.java
+++ android/hyx/src/com/zhaobe/hyx/activity/LoginActivity.java
@@ -1,8 +1,10 @@
-package com.zhaobe.hyx;
+package com.zhaobe.hyx.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.zhaobe.hyx.R;
 
 public class LoginActivity extends Activity {
 
