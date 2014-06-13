457fde74dd810cf307d6d69c7253b9eab9d39174
3203317 <3203317@qq.com>
2014-06-13 15:17:55 +0800

    haha

--- web/public/user/register.js
+++ web/public/user/register.js
@@ -10,52 +10,20 @@ $(function(){
 	showVerifyCode();
 });
 
-$('#btn_reg').click(function(){
+function showVali(valiResu){
+	if('string' === typeof valiResu) return alert(valiResu);
+	$('#regFrm_'+ valiResu[1] +'_Vali').text(valiResu[0]).css('display', 'block').parent().css('display', 'block');
+	$('#regFrm_'+ valiResu[1]).focus();
+}
+
+function subFrm(){
 	$('#regFrm').olxForm('submit', [valiRegFrm, function (valiResu){
 		$('form label[id$="_Vali"]').each(function (i, label){
 			$(label).parent().css('display', 'none');
 		});
-		if('string' === typeof valiResu) return alert(valiResu);
-		$('#regFrm_'+ valiResu[1] +'_Vali').text(valiResu[0]).css('display', 'block').parent().css('display', 'block');
-		$('#regFrm_'+ valiResu[1]).focus();
+		showVali(valiResu);
 	}, function (data){
-		console.log(data);
+		if(data.success) return location.href = 'haha/register/success';
+		showVali(data.msg);
 	}]);
-
-	// var formObj = $("#regFrm").serializeObjectForm();
-	// var valiResu = valiUserRegFrm(formObj);
-	// if(valiResu){
-	// 	if('string' === typeof valiResu){
-	// 		$('#alert_msg').html(valiResu);
-	// 	}else{
-	// 		$('#alert_msg').html(valiResu[0]);
-	// 		$('#regFrm_'+ valiResu[1]).focus();
-	// 	}
-	// 	return;
-	// }
-
-	// $.ajax({
-	// 	url: '/user/register',
-	// 	type: "POST",
-	// 	dataType: "json",
-	// 	data: {
-	// 		data: JSON.stringify(formObj)
-	// 	}
-	// }).done(function(responseText) {
-	// 	var msg, lab;
-	// 	if(responseText.success){
-	// 		location.href = formObj.UserName +'/register/success';
-	// 	}else{
-	// 		msg = responseText.msg;
-	// 		if(msg.message) return $('#alert_msg').html(msg.message);
-
-	// 		if('string' === msg) return $('#alert_msg').html(msg);
-
-	// 		$('#alert_msg').html(msg[0]);
-
-	// 		lab = $('#regFrm_'+ msg[1]);
-	// 		if(lab[0]) lab.focus();
-	// 	}
-	// }).complete(function(){
-	// });
-});
+}
