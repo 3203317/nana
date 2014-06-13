d06b3a8456fe098d6735479f9361060b58dd15c7
3203317 <3203317@qq.com>
2014-06-13 16:13:23 +0800

    haha

---
+++ asset/gen.js
@@ -0,0 +1,2 @@
+java -jar ../../js/yui/2.4.8/yuicompressor-2.4.8.jar ../web/public/user/register.js -o ../web/public/user/register.min.js --charset utf-8
+java -jar ../../js/yui/2.4.8/yuicompressor-2.4.8.jar ../web/public/user/regFrm.js -o ../web/public/user/regFrm.min.js --charset utf-8
