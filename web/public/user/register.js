cef9c3443a0111cc98f92fdebbae28b480cda1a1
3203317 <3203317@qq.com>
2014-06-16 14:31:10 +0800

    haha

--- web/public/user/register.js
+++ web/public/user/register.js
@@ -11,19 +11,19 @@ $(function(){
 });
 
 function showVali(valiResu){
+	$('form label[id$="_Vali"]').each(function (i, label){
+		$(label).parent().css('display', 'none');
+	});
 	if('string' === typeof valiResu) return alert(valiResu);
 	$('#regFrm_'+ valiResu[1] +'_Vali').text(valiResu[0]).css('display', 'block').parent().css('display', 'block');
 	$('#regFrm_'+ valiResu[1]).focus();
 }
 
 function subFrm(){
-	$('#regFrm').olxForm('submit', [function(){}, function (valiResu){
-		// $('form label[id$="_Vali"]').each(function (i, label){
-		// 	$(label).parent().css('display', 'none');
-		// });
-		// showVali(valiResu);
-	}, function (data){
-		if(data.success) return location.href = 'haha/register/success';
-		showVali(data.msg);
+	$('#regFrm').olxForm('submit', [function(){}, function (valiResu){}, function (data){
+		if(!data.success) return showVali(data.msg);
+		location.href = $('#regFrm_UserName').val().trim() +'/register/success';
 	}]);
 }
+
+$('#btn_reg').click(subFrm);
