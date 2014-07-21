0f116c13b1cc649bca85ff508a22be501785b5f5
3203317 <3203317@qq.com>
2014-07-21 11:19:29 +0800

    haha

--- blog/controllers/user.js
+++ blog/controllers/user.js
@@ -10,6 +10,7 @@ exports.loginUI = function(req, res, next){
 		description: '用户管理',
 		keywords: ',用户管理,Bootstrap3',
 		virtualPath: virtualPath,
+		refererUrl: escape('http://'+ req.headers.host + req.url),
 		cdn: conf.cdn
 	});
 };
