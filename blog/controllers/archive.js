b3b4ea787db1c4cf2cc7a346b6cae49afac3960e
3203317 <3203317@qq.com>
2014-07-29 10:00:31 +0800

    haha

--- blog/controllers/archive.js
+++ blog/controllers/archive.js
@@ -4,8 +4,6 @@ var conf = require('../settings'),
 var title = 'FOREWORLD 洪荒',
 	virtualPath = '/';
 
-var Article = require('../biz/article');
-
 function getTopMessage(){
 	var time = new Date();
 	var year = time.getFullYear();
@@ -15,88 +13,14 @@ function getTopMessage(){
 };
 
 exports.index = function(req, res, next){
-	Article.findAll(function (err, status, msg, docs){
-		if(err) return next(err);
-
-		/* 生成档案馆对象 */
-		var archives = [],
-			archive,
-			articles = docs,
-			article,
-			archiveChild;
-
-		for(var i in articles){
-			article = articles[i];
-
-			if(archives.length){
-				/* 获取最后一条记录年 */
-				archive = archives[archives.length - 1];
-
-				if(article.PostTime.getFullYear() == archive.Y4){
-					/* 获取最后一条记录月 */
-					archiveChild = archive.ArchiveChildren[archive.ArchiveChildren.length - 1];
-					if(article.PostTime2Month == archiveChild.M2){
-						archiveChild.Articles.push(article);
-					}else{
-						archiveChild = {
-							'M2': article.PostTime2Month,
-							'Articles': []
-						};
-
-						archiveChild.Articles.push(article);
-						archive.ArchiveChildren.push(archiveChild);
-					}
-
-				}else{
-					/* 添加年 */
-					archive = {
-						'Y4': article.PostTime.getFullYear(),
-						'ArchiveChildren': []
-					};
-
-					/* 添加月 */
-					archiveChild = {
-						'M2': article.PostTime2Month,
-						'Articles': []
-					}
-
-					archiveChild.Articles.push(article);
-
-					archive.ArchiveChildren.push(archiveChild);
-
-					archives.push(archive);
-				}
-			}else{
-				/* 添加年 */
-				archive = {
-					'Y4': article.PostTime.getFullYear(),
-					'ArchiveChildren': []
-				};
-
-				/* 添加月 */
-				archiveChild = {
-					'M2': article.PostTime2Month,
-					'Articles': []
-				}
-
-				archiveChild.Articles.push(article);
-
-				archive.ArchiveChildren.push(archiveChild);
-
-				archives.push(archive);
-			}
-		}
-
-		res.render('Archive', {
-			moduleName: 'archive',
-			title: title,
-			atitle: '档案馆',
-			description: '档案馆',
-			keywords: ',档案馆,Bootstrap3',
-			virtualPath: virtualPath,
-			topMessage: getTopMessage(),
-			cdn: conf.cdn,
-			archives: archives
-		});
+	res.render('Archive', {
+		moduleName: 'archive',
+		title: title,
+		atitle: '档案馆',
+		description: '档案馆',
+		keywords: ',档案馆,Bootstrap3',
+		virtualPath: virtualPath,
+		topMessage: getTopMessage(),
+		cdn: conf.cdn
 	});
 };
--- blog/controllers/site.js
+++ blog/controllers/site.js
@@ -18,8 +18,8 @@ var title = 'FOREWORLD 洪荒',
 exports.installUI = function(req, res, next){
 	var vmPath = path.join(cwd, 'views', 'pagelet');
 
-	var ep = EventProxy.create('topNavCategory', 'usefulLinks', 'top10Comments', 'top10ViewNums',
-		function (topNavCategory, usefulLinks, top10Comments, top10ViewNums){
+	var ep = EventProxy.create('topNavCategory', 'usefulLinks', 'top10Comments', 'top10ViewNums', 'archiveList',
+		function (topNavCategory, usefulLinks, top10Comments, top10ViewNums, archiveList){
 			res.send({
 				success: true,
 				data: arguments
@@ -29,6 +29,90 @@ exports.installUI = function(req, res, next){
 
 	ep.fail(function (err){
 		next(err);
+	});
+
+	/* 档案馆 */
+	Article.findAll(function (err, status, msg, docs){
+		if(err) return ep.emit('error', err);
+
+		/* 生成档案馆对象 */
+		var archives = [],
+			archive,
+			articles = docs,
+			article,
+			archiveChild;
+
+		for(var i in articles){
+			article = articles[i];
+
+			if(archives.length){
+				/* 获取最后一条记录年 */
+				archive = archives[archives.length - 1];
+
+				if(article.PostTime.getFullYear() == archive.Y4){
+					/* 获取最后一条记录月 */
+					archiveChild = archive.ArchiveChildren[archive.ArchiveChildren.length - 1];
+					if(article.PostTime2Month == archiveChild.M2){
+						archiveChild.Articles.push(article);
+					}else{
+						archiveChild = {
+							'M2': article.PostTime2Month,
+							'Articles': []
+						};
+
+						archiveChild.Articles.push(article);
+						archive.ArchiveChildren.push(archiveChild);
+					}
+
+				}else{
+					/* 添加年 */
+					archive = {
+						'Y4': article.PostTime.getFullYear(),
+						'ArchiveChildren': []
+					};
+
+					/* 添加月 */
+					archiveChild = {
+						'M2': article.PostTime2Month,
+						'Articles': []
+					}
+
+					archiveChild.Articles.push(article);
+					archive.ArchiveChildren.push(archiveChild);
+					archives.push(archive);
+				}
+			}else{
+				/* 添加年 */
+				archive = {
+					'Y4': article.PostTime.getFullYear(),
+					'ArchiveChildren': []
+				};
+
+				/* 添加月 */
+				archiveChild = {
+					'M2': article.PostTime2Month,
+					'Articles': []
+				}
+
+				archiveChild.Articles.push(article);
+				archive.ArchiveChildren.push(archiveChild);
+				archives.push(archive);
+			}
+		}
+
+		fs.readFile(path.join(vmPath, 'ArchiveList.vm.html'), 'utf8', function (err, template){
+			if(err) return ep.emit('error', err);
+
+			var html = velocity.render(template, {
+				virtualPath: virtualPath,
+				archiveList: archives
+			});
+
+			fs.writeFile(path.join(vmPath, 'html', 'archiveList.html'), html, 'utf8', function (err){
+				if(err) return ep.emit('error', err);
+				ep.emit('archiveList', true);
+			});
+		});
 	});
 
 	/* 热门文章前10 */
