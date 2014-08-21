417ea2fefed52283fd667e7b3bdb433da978eeec
3203317 <3203317@qq.com>
2014-08-21 22:02:43 +0800

    haha

--- grunt-blog/Gruntfile.js
+++ grunt-blog/Gruntfile.js
@@ -6,11 +6,12 @@ module.exports = function(grunt){
 			options: {
 				banner: '/*!\n * <%= pkg.name %>\n * <%= pkg.author %>\n * <%= grunt.template.today("yyyy-mm-dd") %>\n */\n'
 			}, build: {
-				src: 'src/*.js',
-				dest: 'build/<%= pkg.name %>.js'
+				src: 'src/package.json',
+				dest: 'build/package.json'
 			}, buildall: { //任务三：按原文件结构压缩js文件夹内所有JS文件
 				files: [{
-					expand: true,
+					mangle: !0, //不混淆变量名
+					expand: !0,
 					cwd: 'src', //js目录下
 					src: '**/*.js', //所有js文件
 					dest: 'build/' //输出到此目录下
