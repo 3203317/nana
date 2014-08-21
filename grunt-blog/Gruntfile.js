ac5da8813ec56ee197ae1a00e131c674034f47c7
3203317 <3203317@qq.com>
2014-08-21 21:26:57 +0800

    haha

--- grunt-blog/Gruntfile.js
+++ grunt-blog/Gruntfile.js
@@ -4,10 +4,17 @@ module.exports = function(grunt){
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '/*!\n * <%= pkg.name %>\n * <%= pkg.author %>\n * <%= grunt.template.today("yyyy-mm-dd") %>\n */\n'
 			}, build: {
-				src: 'src/<%= pkg.name %>.js',
-				dest: 'build/<%= pkg.name %>.min.js'
+				src: 'src/*.js',
+				dest: 'build/<%= pkg.name %>.js'
+			}, buildall: { //任务三：按原文件结构压缩js文件夹内所有JS文件
+				files: [{
+					expand: true,
+					cwd: 'src', //js目录下
+					src: '**/*.js', //所有js文件
+					dest: 'build/' //输出到此目录下
+				}]
 			}
 		}
 	});
@@ -16,5 +23,6 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify']);
+	// grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['uglify:buildall']);
 };
