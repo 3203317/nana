d83a817f6c737303a5ea8834eb7134493c544f87
3203317 <3203317@qq.com>
2014-09-06 18:45:26 +0800

    haha

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -11,60 +11,6 @@ var virtualPath = '',
 	title = 'FOREWORLD 洪荒',
 	str1 = '参数异常';
 
-/**
- * post数据校验
- *
- * @params {Object} 
- * @params {Object} 
- * @return {Object} 
- */
-function valiPostData(req, res, next){
-	var data = req.body.data;
-	if(!data) return res.send({
-		success: false,
-		msg: str1
-	});
-
-	try{
-		data = JSON.parse(data);
-		if('object' === typeof data){
-			req._data = data;
-			return next();
-		}
-		res.send({
-			success: false,
-			msg: str1
-		});
-	}catch(ex){
-		res.send({
-			success: false,
-			msg: ex.message
-		});
-	}
-}
-
-/**
- * get数据校验
- *
- * @params {Object} 
- * @params {Object} 
- * @return {Object} 
- */
-function valiGetData(req, res, next){
-	var data = req.query.data;
-	if(!data) return next(new Error(str1));
-	try{
-		data = JSON.parse(data);
-		if('object' === typeof data){
-			req._data = data;
-			return next();
-		}
-		next(new Error(str1));
-	}catch(ex){
-		next(new Error(ex.message));
-	}
-}
-
 module.exports = function(app){
 	app.get('/index.html$', site.indexUI);
 	app.get('/index/more$', site.indexUI_more);
@@ -119,3 +65,57 @@ module.exports = function(app){
 	app.post('/manage/article/tag/del$', valiPostData, manager.validate, tag.removes);
 	app.post('/manage/article/tag/edit$', valiPostData, manager.validate, tag.edit);
 };
+
+/**
+ * post数据校验
+ *
+ * @params {Object} 
+ * @params {Object} 
+ * @return {Object} 
+ */
+function valiPostData(req, res, next){
+	var data = req.body.data;
+	if(!data) return res.send({
+		success: false,
+		msg: str1
+	});
+
+	try{
+		data = JSON.parse(data);
+		if('object' === typeof data){
+			req._data = data;
+			return next();
+		}
+		res.send({
+			success: false,
+			msg: str1
+		});
+	}catch(ex){
+		res.send({
+			success: false,
+			msg: ex.message
+		});
+	}
+}
+
+/**
+ * get数据校验
+ *
+ * @params {Object} 
+ * @params {Object} 
+ * @return {Object} 
+ */
+function valiGetData(req, res, next){
+	var data = req.query.data;
+	if(!data) return next(new Error(str1));
+	try{
+		data = JSON.parse(data);
+		if('object' === typeof data){
+			req._data = data;
+			return next();
+		}
+		next(new Error(str1));
+	}catch(ex){
+		next(new Error(ex.message));
+	}
+}
