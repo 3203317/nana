8921ad6238a0dcec38b3b40580ccff0d3069a2f3
unknown <3203317@qq.com>
2015-08-16 10:16:27 +0800

    haha

---
+++ blog/settings.js
@@ -0,0 +1,36 @@
+/*!
+ * blog
+ * Copyright(c) 2015 foreworld.net <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+module.exports = {
+	cookie: {
+		secret: 'foreworld'
+	}, corp: {
+		name: 'FOREWORLD 洪荒',
+		website: 'http://www.foreworld.net/'
+	}, db: {
+		database: 'foreworld',
+		host: 'www.foreworld.net',
+		port: 27017,
+		user: 'sa',
+		pass: 'xiang123'
+	}, html: {
+		cdn: 'http://www.foreworld.net/js/',
+		static_res: '/public/',
+		external_res: 'http://www.foreworld.net/public/',
+		pagesize: 10,
+		cache_time: 1000 * 60 * 60
+	}, mail: {
+		secureConnection: true,
+		host: 'smtp.qq.com',
+		port: 465,
+		to: ['huangxin@foreworld.net', 'fxy_100@sina.com'],
+		auth: {
+			user: '3203317@qq.com',
+			pass: ''
+		}
+	}
+};
