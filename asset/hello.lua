9b5f2db17c7ffbe3e26fbaa5318b8498935c76ed
3203317 <root@iZ28gi7mzknZ.(none)>
2017-01-27 23:42:08 +0800

    haha

--- asset/hello.lua
+++ asset/hello.lua
@@ -3,4 +3,23 @@
 local var = ngx.var;
 
 
-ngx.say('{ "error": { "code": 40001, "msg": "hello, lua!"} }');
+local _timestamp = var.arg_timestamp;
+
+
+
+
+
+
+
+
+
+local util = require "util";
+
+_timestamp = util.isEmpty(_timestamp);
+
+if nil == _timestamp then
+  return ngx.say('{"error":{"code":40001}}');
+end;
+
+
+ngx.say('{"data":{"msg":"hello lua!"}}');
