ff8f800c2a74301ea1aaff8144daff1dc9d5f1de
3203317 <root@iZ28gi7mzknZ.(none)>
2017-01-29 20:43:30 +0800

    haha

--- asset/hello.lua
+++ asset/hello.lua
@@ -2,10 +2,7 @@
 
 local var = ngx.var;
 
-
 local _timestamp = var.arg_timestamp;
-
-
 
 local util = require "util";
 
@@ -16,14 +13,25 @@ if nil == _timestamp then
   return ngx.exit(ngx.HTTP_OK);
 end;
 
+--[[
+tonumber
+--]]
+_timestamp = tonumber(_timestamp);
+
+if nil == _timestamp then
+  ngx.say('{"error":{"code":40001}}');
+  return ngx.exit(ngx.HTTP_OK);
+end;
 
 --[[
 UTC
 --]]
 local _utctime = (ngx.now() - 8 * 60 * 60) * 1000;
 
-
-
+if _timestamp < _utctime then
+  ngx.say('{"error":{"code":40002}}');
+  return ngx.exit(ngx.HTTP_OK);
+end;
 
 ngx.say('{"data":{"msg":"hello lua!"}}');
 ngx.exit(ngx.HTTP_OK);
